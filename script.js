b76eedffe6f34b56f1ac54871824af6c24a17380
YY-otter <90760759+YY-otter@users.noreply.github.com>
2023-03-27 02:28:20 +0900

    delete unnecessary code

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 const ELEMENT_TEXT_INPUT = document.getElementById("textInput");
 const ELEMENT_TEXT_OUTPUT = document.getElementById("textOutput");
-const ELEMENT_CHECKBOX_URA = document.getElementById("checkboxUra");
 const ELEMENT_A_URL_PUZZLINK = document.getElementById("aURLpuzzlink");
-const ELEMENT_A_URL_PZPRRT = document.getElementById("aURLpzprrt");
 
 function ConvertToURL() {
   const INPUT_TEXT = ELEMENT_TEXT_INPUT.value.replaceAll(/\s+/g, '');
@@ -10,17 +8,11 @@ function ConvertToURL() {
 
   const DEVIDED_INPUT_TEXT = DevideText(INPUT_TEXT, INTERVAL);
 
-  //console.log(DEVIDED_INPUT_TEXT);
-
   const OUTPUT_TEXT = DataToString(DEVIDED_INPUT_TEXT, INTERVAL);
-
-  //console.log(OUTPUT_TEXT);
 
   ELEMENT_TEXT_OUTPUT.value = OUTPUT_TEXT;
   ELEMENT_A_URL_PUZZLINK.href = "https://puzz.link/p?mashu/6/8/" + OUTPUT_TEXT;
   ELEMENT_A_URL_PUZZLINK.innerHTML = "https://puzz.link/p?mashu/6/8/" + OUTPUT_TEXT;
-  ELEMENT_A_URL_PZPRRT.href = "https://semiexp.net/pzprrt/p.html?mashu/6/8/" + OUTPUT_TEXT;
-  ELEMENT_A_URL_PZPRRT.innerHTML = "https://semiexp.net/pzprrt/p.html?mashu/6/8/" + OUTPUT_TEXT;
 }
 
 function DevideText(INPUT_TEXT, INTERVAL) {
@@ -35,11 +27,11 @@ function DataToString(DEVIDED_INPUT_TEXT, INTERVAL) {
   for(const ELEM of DEVIDED_INPUT_TEXT) {
     for(const STR of ELEM) {
       switch(STR){
-        case "W":
+        case "K":
             temp += "1";
         break;
 
-        case "K":
+        case "W":
             temp += "2";
         break;
 
@@ -47,8 +39,6 @@ function DataToString(DEVIDED_INPUT_TEXT, INTERVAL) {
           temp += "0";
       }
     }
-
-    //console.log(temp);
 
     result += parseInt(temp, 3).toString(27);
 
